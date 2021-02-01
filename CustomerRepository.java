ca5c5ee0586c449d6c83053b0174594866449dc0
cdacaish <68396885+cdacaish@users.noreply.github.com>
2021-02-01 15:56:12 +0530

    Add files via upload

---
+++ CustomerRepository.java
@@ -0,0 +1,49 @@
+package com.cdac.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
+import com.cdac.entity.Customer;
+
+@Repository
+public class CustomerRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void save(Customer customer) {
+		entityManager.merge(customer);
+	}
+	
+	public Customer fetch(int id) {
+		return entityManager.find(Customer.class, id);
+	}
+	
+	public boolean isCustomerPresent(String email) {
+		return (Long)
+				entityManager
+				.createQuery("select count(c.id) from Customer c where c.email = :email") //JPQL/HQL
+				.setParameter("email", email)
+				.getSingleResult() == 1 ? true : false;
+	}
+	
+	//return id of the customer
+	public int fetch(String email, String password) {
+		/*Query q = entityManager.createQuery("select c.id from Customer c where c.email = :email and c.password = :password");
+		q.setParameter("email", email);
+		q.setParameter("password", password);
+		Integer id = (Integer) q.getSingleResult();
+		return id;*/
+		
+		return (Integer)
+				entityManager
+				.createQuery("select c.id from Customer c where c.email = :email and c.password = :password")
+				.setParameter("email", email)
+				.setParameter("password", password)
+				.getSingleResult();
+	}
+}
